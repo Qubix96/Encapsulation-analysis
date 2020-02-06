efaa9b90291a3f90c32be01313c69924f8e5e485
Jakub Kubiś <jakub.kubis1996@gmail.com>
2020-02-06 14:23:48 +0100

    New fraction compare

--- Compare influence rpm on capsules size.R
+++ Compare influence rpm on capsules size.R
@@ -30,7 +30,7 @@ dc100 <- dat_csv1 %>%
   mutate(d, RPM = 100) %>% 
   select(d, RPM)
 
-write.csv(dc100,"Alles 5050\\dc100.csv", row.names = FALSE)
+write.csv(dc100,"currentdata/AllData/dc100.csv", row.names = FALSE)
 
 # 200 RPM 55
 
@@ -50,7 +50,7 @@ dc200 <- dat_csv2 %>%
   select(d, RPM)
 
 
-write.csv(dc200,"Alles 5050\\dc200.csv", row.names = FALSE)
+write.csv(dc200,"currentdata/AllData/dc200.csv", row.names = FALSE)
 
 # 300 RPM 55
 
@@ -70,7 +70,7 @@ dc300 <- dat_csv3 %>%
   select(d, RPM)
 
 
-write.csv(dc300,"Alles 5050\\dc300.csv", row.names = FALSE)
+write.csv(dc300,"currentdata/AllData/dc300.csv", row.names = FALSE)
 
 # 400 RPM 55
 
@@ -90,116 +90,18 @@ dc400 <- dat_csv4 %>%
   select(d, RPM)
 
 
-write.csv(dc400,"data/Alles 5050\\dc400.csv", row.names = FALSE)
+write.csv(dc400,"currentdata/AllData/dc400.csv", row.names = FALSE)
 
-# 100 RPM 73
-
-mydir5 = "100 7030 1"
+mydir5 = "currentdata/AllData/"
 myfiles5 = list.files(path=mydir5, pattern="*.csv", full.names=TRUE)
-myfiles5
+Alles <- ldply(myfiles5, read_csv)
 
-dat_csv5 = ldply(myfiles5, read_csv)
-dat_csv5
-
-
-dc100 <- dat_csv5 %>% 
-  mutate(Rq = Area/3,141592) %>% 
-  mutate(d = sqrt(Rq)*2) %>% 
-  filter(d >=1) %>% 
-  mutate(d, RPM = 100) %>% 
-  select(d, RPM)
-
-
-write.csv(dc100,"Alles 7030\\dc100.csv", row.names = FALSE)
-
-# 200 RPM 73
-
-mydir6 = "200 7030 1"
-myfiles6 = list.files(path=mydir6, pattern="*.csv", full.names=TRUE)
-myfiles6
-
-dat_csv6 = ldply(myfiles6, read_csv)
-dat_csv6
-
-
-dc200 <- dat_csv6 %>% 
-  mutate(Rq = Area/3,141592) %>% 
-  mutate(d = sqrt(Rq)*2) %>% 
-  filter(d >=1) %>% 
-  mutate(d, RPM = 200) %>% 
-  select(d, RPM)
-
-write.csv(dc200,"Alles 7030\\dc200.csv", row.names = FALSE)
-
-# 300 RPM 73
-
-mydir7 = "300 7030 1"
-myfiles7 = list.files(path=mydir7, pattern="*.csv", full.names=TRUE)
-myfiles7
-
-dat_csv7 = ldply(myfiles7, read_csv)
-dat_csv7
-
-
-dc300 <- dat_csv7 %>% 
-  mutate(Rq = Area/3,141592) %>% 
-  mutate(d = sqrt(Rq)*2) %>% 
-  filter(d >=1) %>% 
-  mutate(d, RPM = 300) %>% 
-  select(d, RPM)
-
-write.csv(dc300,"Alles 7030\\dc300.csv", row.names = FALSE)
-
-# 400 RPM 73
-
-mydir8 = "400 7030 1"
-myfiles8 = list.files(path=mydir8, pattern="*.csv", full.names=TRUE)
-myfiles8
-
-dat_csv8 = ldply(myfiles8, read_csv)
-dat_csv8
-
-
-dc400 <- dat_csv8 %>% 
-  mutate(Rq = Area/3,141592) %>% 
-  mutate(d = sqrt(Rq)*2) %>% 
-  filter(d >=1) %>% 
-  mutate(d, RPM = 400) %>% 
-  select(d, RPM)
-
-write.csv(dc400,"Alles 7030\\dc400.csv", row.names = FALSE)
-
-# Dla wszytkich pomiarów PBS DLS 50 50
-
-mydir9 = "Alles 5050"
-myfiles9 = list.files(path=mydir9, pattern="*.csv", full.names=TRUE)
-myfiles9
-
-alles_5050 = ldply(myfiles9, read_csv)
-alles_5050
-
-# Dla wszytskich pomiraów PBS DLS 70 30
-
-mydir10 = "Alles 7030"
-myfiles10 = list.files(path=mydir10, pattern="*.csv", full.names=TRUE)
-myfiles10
-
-alles_7030 = ldply(myfiles10, read_csv)
-alles_7030
-
-
-q <- ggplot(data = alles_5050, aes(x=as.character(RPM), y=d, fill=RPM)) + # fill=name allow to automatically dedicate a color for each group
+ggplot(data = Alles, aes(x=as.character(RPM), y=d, fill=RPM)) + # fill=name allow to automatically dedicate a color for each group
   geom_violin(scale = "area")+
-  xlab("RPM")+
-  ylab("Capsule size [µm]")+
+  xlab("rpm")+
+  ylab("Wielkość mikrosfer [µm]")+
   theme(legend.position = "none")+
-  ggtitle("Capsule density depending on the mixing speed for PBS50 / DLS50")
+  ggtitle("Zależność wielkości kapsułek od prędkości mieszania W/O")
 
-
-w <- ggplot(data = alles_7030, aes(x=as.character(RPM), y=d, fill=RPM)) + # fill=name allow to automatically dedicate a color for each group
-  geom_violin(scale = "area")+
-  xlab("RPM")+
-  ylab("Capsule size [µm]")+
-  theme(legend.position = "none")+
-  ggtitle("Capsule density depending on the mixing speed for PBS70 / DLS30")
+ggsave(filename = paste0("figures/",PVA,"",file,"rpmsize.png"), dpi = 600)
 
